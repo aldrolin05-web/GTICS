60919826c036239c5ceaeecc5ec99f180e4f29f5
Luis Alejandro Lingan Silva <aldrolin05@gmail.com>
2026-04-17 08:24:37 -0500

    primer commit

---
+++ src/main/java/com/example/primeraprueba/PrimeraPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeraprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeraPruebaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/primeraprueba/PrimeraPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeraprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraPruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
